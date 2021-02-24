26e2a20c8f55cd3cca2531460c42b97a41aac37c
Niko <nikosheng@gmail.com>
2021-02-24 15:48:47 +0800

    add hot items kafka sink

--- ecommerce-aws-flink-hotitems/src/main/java/flink/aws/hotitems/StreamingHotItems.java
+++ ecommerce-aws-flink-hotitems/src/main/java/flink/aws/hotitems/StreamingHotItems.java
@@ -32,7 +32,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Timestamp;
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Properties;
